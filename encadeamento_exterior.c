1eba85c00b62fb65cb308ba32d7abfb850554532
Idyl Icaro dos Santos <idylicaro.se@hotmail.com>
2020-12-12 23:23:22 -0300

    refactor: using buscar in excluir

--- encadeamento_exterior.c
+++ encadeamento_exterior.c
@@ -79,16 +79,15 @@ int insere(int cod_cli, char *nome_cli, char *nome_arquivo_hash, char *nome_arqu
 
 int exclui(int cod_cli, char *nome_arquivo_hash, char *nome_arquivo_dados)
 {
+    int posClient = -1;
+    // busca a posição do cliente na hashtable
+    posClient = busca(cod_cli, nome_arquivo_hash, nome_arquivo_dados);
+
     FILE *in = fopen(nome_arquivo_hash, "rb");
     FILE *out = fopen(nome_arquivo_dados, "rb+");
     if (in == NULL || out == NULL){
         printf("So sorry, i can't open this archive");
     }else{
-        int posClient = -1;
-        int hashCod = funcHash(cod_cli,7);
-        // busca a posição do cliente na hashtable
-        fseek(in, sizeof(int) * hashCod, SEEK_SET);
-        fread(&posClient, sizeof(int), 1, in);
 
         if(posClient == -1){
             return -1;
@@ -96,16 +95,6 @@ int exclui(int cod_cli, char *nome_arquivo_hash, char *nome_arquivo_dados)
 
         fseek(out, tamanho_cliente() * posClient, SEEK_SET);
         Cliente *c = le_cliente(out);
-        // verifica se é o cliente se não verifica se tem proximo
-        while (c->cod_cliente != cod_cli && c->prox != -1){
-            fseek(out, tamanho_cliente() * c->prox, SEEK_SET);
-            posClient = c->prox;
-            c = le_cliente(out);
-        }
-
-        if (c->prox == -1 && c->cod_cliente!= cod_cli){ // cliente não existe
-            return -1;
-        }
 
         c->status = LIBERADO;
         fseek(out, sizeof(Cliente) * posClient, SEEK_SET);
